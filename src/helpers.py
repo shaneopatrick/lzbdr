5894910a27c9d30ac214097455be8138e39a1f22
Shane Milek <spmilek@gmail.com>
2017-08-23 21:13:40 -0600

    add helper function

--- src/helpers.py
+++ src/helpers.py
@@ -81,22 +81,58 @@ def preprocess_image(filepath):
     return image
 
 
+def crop_image(filepath, detection_graph):
+    img_full_path = '../z_app/uploads/{}'.format(filepath)
+    image_np = cv2.imread(img_full_path)
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+            # Set mask for only bird objects
+            mask = np.argwhere(classes[0] == 16.)
+            if len(mask) < 1:
+                return None, None, None
+            score1 =  scores[0][mask[0]].ravel()
+            classes1 = classes[0][mask[0]].ravel()
+            box1 = boxes[0][mask[0]].ravel()
+            # print('Box1: ', box1)
+            y_min = box1[0]
+            x_min = box1[1]
+            y_max = box1[2]
+            x_max = box1[3]
+
+            im_height, im_width,  _ = image_np.shape
+            coords = (abs(int(y_min * im_height)-20), int(y_max * im_height)+20, abs(int(x_min * im_width)-20), int(x_max * im_width)+20)
+
+            # print('\nCoords: ', coords)
+
+            # cv2.imshow('test', image_np)
+            # cv2.waitKey(3000)
+
+            new_img = image_np[coords[0]:coords[1], coords[2]:coords[3]]
+            # cv2.imwrite('vis_util.png', image_np)
+            # cv2.imwrite('cropped.png', new_img)
+            # cv2.imshow('cropped', new_img)
+            # cv2.waitKey(3000)
+            return new_img, score1, classes1
+
+
 def load_dict():
     with open('../data/top200_dict.pkl', 'rb') as f:
         b_dict = pickle.load(f)
         return b_dict
-
-
-def return_top_n(pred_arr, n=3):
-    '''
-    IN: flattened prediction array
-    OUT: top n indices (dict keys) and percentages
-    '''
-    sorts = np.argsort(-pred_arr)
-    topn_keys = sorts[:n]
-    percents = pred_arr[topn_keys]
-    return topn_keys, percents
-
 
 ### Measure Accuracy
 
